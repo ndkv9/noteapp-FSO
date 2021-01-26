2ed9233c88aba384afd49e065984ba779caa359f
Vu Nguyen <vu.nguyen101@gmail.com>
2021-01-26 20:24:18 +0200

    add addNote fn

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import Note from './components/Note'
 
 const App = props => {
 	const [notes, setNotes] = useState(props.notes)
+	const [newNote, setNewNote] = useState('')
+
+	const addNote = e => {
+		e.preventDefault()
+		const noteObj = {
+			content: newNote,
+			date: new Date().toISOString(),
+			important: Math.random() < 0.5,
+			id: notes.length + 1,
+		}
+
+		setNotes(notes.concat(noteObj))
+		setNewNote('')
+	}
+
+	const handleNoteChange = e => {
+		setNewNote(e.target.value)
+	}
 
 	return (
 		<div>
@@ -12,6 +30,10 @@ const App = props => {
 					<Note key={note.id} content={note.content} />
 				))}
 			</ul>
+			<form onSubmit={addNote}>
+				<input value={newNote} onChange={handleNoteChange} />
+				<button type='submit'>save</button>
+			</form>
 		</div>
 	)
 }
