b35a39cd67fa18bbf9127306d5b40b57897f0ece
Vu Nguyen <vu.nguyen101@gmail.com>
2021-01-26 14:41:49 +0200

    add Note component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-const App = ({ notes }) => {
+import Note from './components/Note'
+
+const App = () => {
+	const notes = [
+		{
+			id: 1,
+			content: 'HTML is easy',
+			date: '2019-05-30T17:30:31.098Z',
+			important: true,
+		},
+		{
+			id: 2,
+			content: 'Browser can execute only JavaScript',
+			date: '2019-05-30T18:39:34.091Z',
+			important: false,
+		},
+		{
+			id: 3,
+			content: 'GET and POST are the most important methods of HTTP protocol',
+			date: '2019-05-30T19:20:14.298Z',
+			important: true,
+		},
+	]
+
 	return (
 		<div>
 			<h1>Notes</h1>
 			<ul>
-				<li>{notes[0].content}</li>
-				<li>{notes[1].content}</li>
-				<li>{notes[2].content}</li>
+				{notes.map(note => (
+					<Note key={note.id} content={note.content} />
+				))}
 			</ul>
 		</div>
 	)
