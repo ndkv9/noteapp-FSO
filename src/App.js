4ed8c1ef363884f525043c58814d6934e831d64c
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-02 11:47:53 +0200

    add note services

--- src/App.js
+++ src/App.js
@@ -37,12 +37,26 @@ const App = () => {
 
 	const notesToShow = showAll ? notes : notes.filter(n => n.important)
 
+	const toggleImportant = id => {
+		const url = `http://localhost:5000/notes/${id}`
+		const note = notes.find(n => n.id === id)
+		const changedNote = { ...note, important: !note.important }
+
+		axios.put(url, changedNote).then(res => {
+			setNotes(notes.map(note => (note.id !== id ? note : res.data)))
+		})
+	}
+
 	return (
 		<div>
 			<h1>Notes</h1>
 			<ul>
 				{notesToShow.map(note => (
-					<Note key={note.id} content={note.content} />
+					<Note
+						key={note.id}
+						note={note}
+						toggleImportant={() => toggleImportant(note.id)}
+					/>
 				))}
 			</ul>
 			<button onClick={handleImportance}>{btnText}</button>
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,5 +1,11 @@
-const Note = ({ content }) => {
-	return <li>{content}</li>
+const Note = ({ note, toggleImportant }) => {
+	const label = note.important ? 'make not important' : 'make important'
+	return (
+		<li>
+			{note.content}
+			<button onClick={toggleImportant}>{label}</button>
+		</li>
+	)
 }
 
 export default Note
