68c1af5a3127b55c9996868934f6271734037275
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-03 13:18:24 +0200

    add styles to css file

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,7 +1,7 @@
 const Note = ({ note, toggleImportant }) => {
 	const label = note.important ? 'make not important' : 'make important'
 	return (
-		<li>
+		<li className='note'>
 			{note.content}
 			<button onClick={toggleImportant}>{label}</button>
 		</li>
