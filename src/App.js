506fa4ccc7fe42e2b2dfa71d364c05a189897a23
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-03 13:31:37 +0200

    add errorMessage to App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
+import Noti from './components/Noti'
 import noteService from './services/notes'
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
 	const [newNote, setNewNote] = useState('')
 	const [showAll, setShowAll] = useState(true)
 	const [btnText, setBtnText] = useState('show important')
+	const [errorMessage, setErrorMessage] = useState(null)
 
 	useEffect(() => {
 		noteService.getAll().then(initialNotes => setNotes(initialNotes))
@@ -47,7 +49,10 @@ const App = () => {
 				setNotes(notes.map(note => (note.id !== id ? note : returnedNote)))
 			})
 			.catch(error => {
-				alert(`the note '${note.content}' was already deleted from server`)
+				setErrorMessage(error.message)
+				setTimeout(() => {
+					setErrorMessage(null)
+				}, 3000)
 				setNotes(notes.filter(n => n.id !== id))
 			})
 	}
@@ -55,6 +60,7 @@ const App = () => {
 	return (
 		<div>
 			<h1>Notes</h1>
+			<Noti message={errorMessage} />
 			<ul>
 				{notesToShow.map(note => (
 					<Note
