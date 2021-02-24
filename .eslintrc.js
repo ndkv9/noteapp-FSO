530934b4d9bc354d249d6de882ab166ea0f59af4
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-24 13:53:44 +0200

    I hate ESlint..

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,37 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    "jest/globals": true,
-  },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  plugins: ["react", "jest"],
-  rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix", "windows"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
-    eqeqeq: "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", { before: true, after: true }],
-    "no-console": 0,
-    "react/prop-types": 0,
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-};
+	env: {
+		browser: true,
+		es6: true,
+		'jest/globals': true,
+	},
+	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['react', 'jest'],
+	rules: {
+		'no-undef': ['error'],
+		'no-unused-vars': 'error',
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'windows'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		eqeqeq: 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': ['error', 'always'],
+		'arrow-spacing': ['error', { before: true, after: true }],
+		'no-console': 0,
+		'react/prop-types': 0,
+		'react/react-in-jsx-scope': 0,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+}
