14245bdc2423c6d33026f9e09b322b65f2bad6c2
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-26 14:32:18 +0200

    add test case for user can log in

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -1,12 +1,23 @@
 describe('Note app', function () {
-	it('front page can be opened', function () {
+	beforeEach(function () {
 		cy.visit('http://localhost:3000')
+	})
+
+	it('front page can be opened', function () {
 		cy.contains('Notes')
 		cy.contains('Note App, by Erik Vu Nguyen')
 	})
 
 	it('login form can be opened', function () {
-		cy.visit('http://localhost:3000')
 		cy.contains('login').click()
+	})
+
+	it('user can login', function () {
+		cy.contains('login').click()
+		cy.get('#username').type('saiyan1')
+		cy.get('#password').type('password1')
+		cy.get('#login-btn').click()
+
+		cy.contains('Create a new note')
 	})
 })
